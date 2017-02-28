aa3db57fc654df197b1cdcfea0e32c8b0783fdc5
Zouzou92 <zouzou92000@hotmail.fr>
2017-02-28 16:19:34 +0100

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,9 +1,26 @@
 var http = require('http');
 var url = require('url');
 
+function isPremier(nombre) { // Renvoie true si le nombre est premier, false sinon
+	i = 0;
+	for(i=2;i<=nombre;i++) {
+		if(nombre%i==0&&i!=nombre) {
+			return false;
+		}
+		else if(i==nombre) {
+			return true;
+		}
+		else {
+			
+		}
+	}
+}
+
+
 var server = http.createServer(function (requete, reponse) {
 		var urlpage = url.parse(requete.url).pathname;
 		reponse.writeHead(200,{"Content-Type": "text/plain; charset=utf-8"});
+		urlpage = urlpage.substring(1); // Retire le / au début de la chaine de l'url
 		reponse.end(urlpage);
 });
 
